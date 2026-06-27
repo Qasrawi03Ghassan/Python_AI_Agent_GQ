776e1cb1e0d5f63e4c2a41ededcbd20c427ba1ab
Qasrawi03Ghassan <gqasrawig@gmail.com>
2026-06-27 18:23:55 +0300

    Added tests for internal files like main.py in calculator and other expected to fail tests

--- test_get_file_content.py
+++ test_get_file_content.py
@@ -7,3 +7,12 @@ print(result)
 print(f"lorem.txt length: {len(result)}")
 print(f"lorem.txt truncated: {'truncated' in result}")
 
+print('\n')
+
+print(get_file_content("calculator", "main.py"))
+print('\n')
+print(get_file_content("calculator", "pkg/calculator.py"))
+print('\n')
+print(get_file_content("calculator", "/bin/cat"))
+print('\n')
+print(get_file_content("calculator", "pkg/does_not_exist.py"))

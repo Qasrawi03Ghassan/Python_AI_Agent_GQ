d31139fa45bcbd20cd14457d2587f8c28ee11726
Qasrawi03Ghassan <gqasrawig@gmail.com>
2026-06-27 17:43:13 +0300

    Feat: Added getting files only within permissible directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,21 +1,32 @@
 import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
+    print(f'Result for current directory:') if directory == '.' else print(f'Result for \'{directory}\' directory:')
+
     try:
         abs_path_working_directory = os.path.abspath(working_directory)
         target_path = os.path.normpath(os.path.join(abs_path_working_directory,directory))
 
         valid_target_path = os.path.commonpath([abs_path_working_directory,target_path]) == abs_path_working_directory
     except:
-        raise Exception(f'Error: Cannot get files info.')
+        return f'Error: Cannot get files info due to library functions error.'
     
 
     if not valid_target_path:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
-    is_directory = os.path.isdir(directory)
+    is_directory = os.path.isdir(target_path)
 
     if not is_directory:
         return f'Error: "{directory}" is not a directory'
     
-    return f'Success: "{directory}" is within the working directory'
+    target_path_files_names = os.listdir(target_path)
+    files_strings = []
+
+    for item in target_path_files_names:
+        files_strings.append(f' - {item}: file_size={os.path.getsize(f'{target_path}/{item}')} bytes, is_dir={os.path.isdir(f'{target_path}/{item}')}')
+        
+    directory_string = '\n'.join(files_strings)
+
+
+    return directory_string
